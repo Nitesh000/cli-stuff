d833c0867fec23d608d6f9898c2c589ddeca81ce
Nitesh000 <ntudu040@gmail.com>
2023-02-06 18:38:16 +0530

    create a cli tool for first time

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let loop = true;
+
+while (loop) {
+  const answers: {
+    TODO: string;
+    addmore: boolean;
+  } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "TODO",
+      message: "What do you want to do? ",
+    },
+    {
+      type: "confirm",
+      name: "addmore",
+      message: "Do you want to add another stuff? ",
+      default: false,
+    },
+  ]);
+  const { TODO, addmore } = answers;
+  console.log(answers);
+  loop = addmore;
+  if (TODO) {
+    todos.push(TODO);
+  } else {
+    console.log("Kindly add valid input.");
+  }
+}
+if (todos.length > 0) {
+  console.log("Your TODO list: ");
+  todos.forEach((todo, index) => {
+    console.log(`${index + 1}. ${todo}`);
+  });
+} else {
+  console.log("You have no TODOs.");
+}
